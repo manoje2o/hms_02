dfe511a79053b94868862aaa05c070e3f8700472
mb6902 <balamanoj02@gmail.com>
2025-06-11 00:11:47 +0530

    https changed

--- src/Axios.js
+++ src/Axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://e2otech-001-site1.ltempurl.com/api',
+  baseURL: 'https://e2otech-001-site1.ltempurl.com/api',
 });
 
 export default instance;
